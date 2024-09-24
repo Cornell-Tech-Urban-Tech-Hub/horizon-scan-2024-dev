584a8fd1161a49804c0914da5fb67591b4a2255e
benoldenburg <oldenburg2@gmail.com>
2024-09-24 13:59:58 -0400

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,41 +5,41 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
-import { useLocation } from "@reach/router"
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
+import { useLocation } from "@reach/router";
 
 function Seo({ description, meta, title }) {
-  const location = useLocation()
+  const location = useLocation();
 
-  const { site, ogimage } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-        ogimage: allFile(filter: { relativePath: { eq: "horizon-scan.png" } }) {
-          nodes {
-            publicURL
-          }
+  const { site, ogimage } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          author
         }
       }
-    `
-  )
+      ogimage: allFile(filter: { relativePath: { eq: "social-card.png" } }) {
+        nodes {
+          publicURL
+        }
+      }
+    }
+  `);
 
-  const siteTitle = site.siteMetadata.title
-  const siteSubtitle = description ? description : site.siteMetadata.description
-  const siteFullTitle = `${siteTitle}: ${siteSubtitle}`
-  const pageTitle = title ? `${title} | ${siteTitle}` : siteTitle
+  const siteTitle = site.siteMetadata.title;
+  const siteSubtitle = description
+    ? description
+    : site.siteMetadata.description;
+  const siteFullTitle = `${siteTitle}: ${siteSubtitle}`;
+  const pageTitle = title ? `${title} | ${siteTitle}` : siteTitle;
 
-  const ogTitle = title ? title : siteTitle
-  const ogImageURL = ogimage.nodes[0].publicURL
+  const ogTitle = title ? title : siteTitle;
+  const ogImageURL = ogimage.nodes[0].publicURL;
 
   // location.pathname === "/" && console.log("LOCATION IS HOMEPAGE")
   //const pageDescription = description ? description : siteDescription
@@ -47,8 +47,8 @@ function Seo({ description, meta, title }) {
     location.pathname === "/"
       ? siteSubtitle
       : description
-      ? description
-      : siteFullTitle
+        ? description
+        : siteFullTitle;
 
   return (
     <Helmet
@@ -100,21 +100,29 @@ function Seo({ description, meta, title }) {
           content: ogImageURL,
         },
       ].concat(meta)}
-    />
-  )
+    >
+      {process.env.NODE_ENV !== "development" && (
+        <script
+          defer
+          data-domain="futureofurbanai.org"
+          src="https://plausible.io/js/script.js"
+        ></script>
+      )}
+    </Helmet>
+  );
 }
 
 Seo.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
+};
 
 Seo.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string,
-}
+};
 
-export default Seo
+export default Seo;
