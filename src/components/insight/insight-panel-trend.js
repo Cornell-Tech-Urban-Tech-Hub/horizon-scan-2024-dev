7baa4ddd34006ce846b5a0cd4da326f48c723779
benoldenburg <oldenburg2@gmail.com>
2024-11-15 17:09:20 -0500

    Update insight-panel-trend.js

--- src/components/insight/insight-panel-trend.js
+++ src/components/insight/insight-panel-trend.js
@@ -11,12 +11,12 @@ import { generateInsight } from "./generateInsight";
 import generatorOptions from "../../content/generator-options.json";
 import { LoadingIcon } from "./loadingIcon";
 import { lighten } from "polished";
+import { above, below } from "../../styles/utilities/breakpoints";
 
 const OptionsPanel = styled.div`
   margin-top: 1rem;
   margin-bottom: 1rem;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
+  padding: 1rem;
   border-radius: 0.5rem;
   //background: #efefef;
   background: #ffffff;
@@ -24,34 +24,77 @@ const OptionsPanel = styled.div`
   h3 {
     margin: 0;
   }
+
+  .options-grid {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 1rem;
+    ${above.lg} {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
+  .options-button {
+    margin-top: 1rem;
+    display: flex;
+    align-items: stretch;
+  }
 `;
 
 const ResultsPanel = styled.div`
-  margin-top: 1rem;
   margin-bottom: 1rem;
-  padding-bottom: 1rem;
   border-radius: 0.5rem;
   background: #efefef;
+  padding: 1rem;
+  border: 1px solid #ccc;
 `;
 
-const StyledInsightCard = styled.div`
+const StyledInsightResult = styled.div`
   margin-top: 1rem;
-  margin-bottom: 1rem;
-  padding-bottom: 1rem;
   border-radius: 0.5rem;
-  background: #efefef;
 
   .insight-set {
-    border: 1px solid #333;
     border-radius: 0.5rem;
-  }
-
-  .insight-inner {
-    font-size: 0.85rem;
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 1rem;
+    ${above.lg} {
+      grid-template-columns: repeat(3, 1fr);
+    }
   }
 
   .meta {
+    margin-top: 1rem;
     color: #777;
+    font-size: 0.85rem;
+  }
+`;
+
+const StyledInsightCard = styled.div`
+  padding: 1rem;
+  background: #fff;
+  border-left: 4px solid ${(props) => props.theme.colors.primary};
+  h4 {
+    margin-top: 0;
+    font-size: 1.2rem;
+    line-height: 1.4;
+    color: #fff;
+    span {
+      background-color: ${(props) => props.theme.colors.primary};
+      box-shadow:
+        1rem 0 0 ${(props) => props.theme.colors.primary},
+        -1rem 0 0 ${(props) => props.theme.colors.primary};
+      box-decoration-break: clone;
+      -webkit-box-decoration-break: clone;
+    }
+  }
+  .insight-header {
+    ${above.lg} {
+      min-height: 4rem;
+    }
+  }
+
+  .insight-inner {
     font-size: 0.85rem;
   }
 `;
@@ -189,54 +232,49 @@ export const InsightPanel = ({ trend, title, html }) => {
           </Col>
         </Row>
         <Row>
-          <Col size={2}>
+          <Col size={1}>
             <div
               dangerouslySetInnerHTML={{
                 __html: html,
               }}
             />
           </Col>
-          <Col>
+          <Col size={2}>
             <OptionsPanel>
-              <Row>
-                <Col>
-                  <h3>Options</h3>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <FormSelect
-                    id={"sector"}
-                    label={"Sector"}
-                    value={selectedSector}
-                    onChange={(e) => updateSector(e.target.value)}
-                    options={optionsSector}
-                  />
+              <h3>Generator Settings</h3>
+              <div className="options-grid">
+                <FormSelect
+                  id={"sector"}
+                  label={"Sector"}
+                  value={selectedSector}
+                  onChange={(e) => updateSector(e.target.value)}
+                  options={optionsSector}
+                />
+                <FormSelect
+                  id={"occupation"}
+                  label={"Occupation"}
+                  value={selectedOccupation}
+                  onChange={(e) => updateOccupation(e.target.value)}
+                  options={optionsOccupation}
+                />
+                <FormSelect
+                  id={"timeframe"}
+                  label={"Timeframe"}
+                  value={selectedTimeframe}
+                  onChange={(e) => updateTimeframe(e.target.value)}
+                  options={optionsTimeframe}
+                />
+              </div>
 
-                  <FormSelect
-                    id={"occupation"}
-                    label={"Occupation"}
-                    value={selectedOccupation}
-                    onChange={(e) => updateOccupation(e.target.value)}
-                    options={optionsOccupation}
-                  />
-
-                  <FormSelect
-                    id={"timeframe"}
-                    label={"Timeframe"}
-                    value={selectedTimeframe}
-                    onChange={(e) => updateTimeframe(e.target.value)}
-                    options={optionsTimeframe}
-                  />
-                </Col>
-              </Row>
+              <div className="options-button">
+                <ButtonGenerator
+                  className="generate-button"
+                  onClick={() => getInsights()}
+                >
+                  {"Generate Insights"}
+                </ButtonGenerator>
+              </div>
             </OptionsPanel>
-            <ButtonGenerator
-              className="generate-button"
-              onClick={() => getInsights()}
-            >
-              {"Get Insights"}
-            </ButtonGenerator>
           </Col>
         </Row>
         <Row>
@@ -250,13 +288,14 @@ export const InsightPanel = ({ trend, title, html }) => {
               <StyledErrorMessage>{errorMessage}</StyledErrorMessage>
             )}
             {!loading && insights.length > 0 && (
-              <>
-                <InsightCardSynthesis
-                  key={1}
-                  result={insights[0]}
-                  showMeta={true}
-                />
-              </>
+              <ResultsPanel>
+                <Row>
+                  <Col>
+                    <h3>Insights</h3>
+                  </Col>
+                </Row>
+                <InsightResult key={1} result={insights[0]} showMeta={true} />
+              </ResultsPanel>
             )}
             {/* {insights.length > 1 && (
         <>
@@ -274,34 +313,23 @@ export const InsightPanel = ({ trend, title, html }) => {
   );
 };
 
-export const InsightCard = ({ result }) => {
+export const InsightResult = ({ result, showMeta = false }) => {
   let trend = result.settings.body.trend;
   let settings = result.settings.body.generator_settings;
 
   return (
-    <StyledInsightCard>
+    <StyledInsightResult>
       <div className="insight-set">
-        <Row>
-          {result.insights?.map((insight, i) => (
-            <Col>
-              <div className="insight">
-                <h3>{insight.title}</h3>
-                <div className="insight-inner">
-                  <p>
-                    <strong>Challenges:</strong> {insight.challenges}
-                  </p>
-                  <p>
-                    <strong>Innovative Applications:</strong>{" "}
-                    {insight.innovative_applications}
-                  </p>
-                  <p>
-                    <strong>Opportunities:</strong> {insight.opportunities}
-                  </p>
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
+        {result.insights?.map((insight, i) => (
+          <InsightCard insight={insight}>
+            <h4>{insight.title}</h4>
+            <div className="insight-inner">
+              <p>{insight.synthesis}</p>
+            </div>
+          </InsightCard>
+        ))}
+      </div>
+      {showMeta && (
         <div className="meta">
           <Col>
             Request ({result.id}): {result.timestamp}
@@ -316,51 +344,21 @@ export const InsightCard = ({ result }) => {
             </div>
           </Col>
         </div>
-      </div>
-    </StyledInsightCard>
+      )}
+    </StyledInsightResult>
   );
 };
 
-export const InsightCardSynthesis = ({ result, showMeta = false }) => {
-  let trend = result.settings.body.trend;
-  let settings = result.settings.body.generator_settings;
-
+export const InsightCard = ({ insight }) => {
   return (
     <StyledInsightCard>
-      <div className="insight-set">
-        <Row>
-          <Col>
-            <h3>Insights</h3>
-          </Col>
-        </Row>
-        <Row>
-          {result.insights?.map((insight, i) => (
-            <Col>
-              <div className="insight">
-                <h4>{insight.title}</h4>
-                <div className="insight-inner">
-                  <p>{insight.synthesis}</p>
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
-        {showMeta && (
-          <div className="meta">
-            <Col>
-              Request ({result.id}): {result.timestamp}
-              {/* / Time: {result.time} */}
-              <div>
-                Trend: <strong>{trend.title}</strong>
-              </div>
-              <div>
-                Sector: <strong>{settings.sector}</strong> / Occupation:{" "}
-                <strong>{settings.occupation}</strong> / Time frame:{" "}
-                <strong>{settings.time_frame}</strong>
-              </div>
-            </Col>
-          </div>
-        )}
+      <div className="insight-header">
+        <h4>
+          <span>{insight.title}</span>
+        </h4>
+      </div>
+      <div className="insight-inner">
+        <p>{insight.synthesis}</p>
       </div>
     </StyledInsightCard>
   );
